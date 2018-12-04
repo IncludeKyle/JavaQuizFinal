4acd3b81890fa5149e6daec154b2e5c8c14f106a
KyleBlaha <Kyle13Blaha@yahoo.com>
2018-12-03 22:18:50 -0600

    Added extra credit bar graph of performance per chapter

--- src/proctortest/TestAndAnalyze.java
+++ src/proctortest/TestAndAnalyze.java
@@ -2,6 +2,7 @@
 package proctortest;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -14,11 +15,15 @@ public class TestAndAnalyze extends TestUtilities{
     // ====== Instance Variables =====
     // ===============================
     private ArrayList<Question> questionList = new ArrayList<>();
+    private ArrayList<String> listOfChapters = new ArrayList<>(); // Stores each unique test chapter 
+    private int chapterIndex = 0;
+    private int[] chapterScore; // Parallel int array stores each chapter's score
+    private String currentChapter;
 
     private int totalQuestions = 0; // Keeps track of how many questions were asked during test
     private int totalCorrect = 0;
     private double percentScore = 100;
-
+    
 
     // ========================
     // ====== Constructor =====
@@ -46,12 +51,36 @@ public class TestAndAnalyze extends TestUtilities{
 
         int count = 1;
         String userAnswer;
+        boolean setFirstChapter = false;
         Scanner scanner = new Scanner(System.in);
 
         // Display test banner
         displayTestBanner();
+        
+        // Pull out each question object to see how many chapters there will be
+        for (Question question : questionList)
+        {
+            // Convert the StringBuilder chapter to a string
+            String convertedStringBuilder = question.getChapter().toString();
+            
+            // If chapter is not in list, add it to the list
+            if ( !listOfChapters.contains(convertedStringBuilder) )
+            {
+                // Add that chapter to the chapter name list
+                listOfChapters.add(convertedStringBuilder);
+            }
+            
+            // Also set the starting chapter
+            if (!setFirstChapter)
+            {
+                currentChapter = convertedStringBuilder;
+                setFirstChapter = true;
+            }
+        }
+        // Set size of the chapter score array
+        chapterScore = new int[listOfChapters.size()];
 
-        // Pull out each question object from the list
+        // Pull out each question object from the list to give the test
         for (Question question : questionList)
         {
             // This string contains the question #, chapter, and section to display
@@ -60,7 +89,6 @@ public class TestAndAnalyze extends TestUtilities{
                         "[Question #" + count + " in " + question.getChapter() +
                         " - " + question.getSection() + " | Score: (" + totalCorrect + "/" +
                         totalQuestions + ")=" + percentScore + "%]";
-
 
             line(questionStats.length(), '='); // --line--
             System.out.println(questionStats); // Display origin of question to user
@@ -107,20 +135,42 @@ public class TestAndAnalyze extends TestUtilities{
     // chapter and section
     private void analyzeAnswer(String answer, StringBuilder correctAnswer, StringBuilder chapter, StringBuilder section)
     {
+
         // Compare answer and correct answer
         if (correctAnswer.charAt(1) == answer.charAt(0))
         {
-            System.out.println("< Correct! >");
+            System.out.println("< Correct!>");
             totalCorrect++; // Update the score instance variable
+            
+            // If this detects that chapter is != currentChapter 
+            // it knows the chapter has changed and it will update accordingly
+            if ( chapter.toString().equals(currentChapter) )
+            {
+                chapterScore[chapterIndex]++; // Add +1 point to the respective chapter score
+            }
+            else
+            {
+                currentChapter = chapter.toString(); // Update current chapter 
+                chapterIndex++; // Update chapter score index +1 to move to next chapter
+                chapterScore[chapterIndex]++; // Add +1 point to the respective chapter score
+            }
+        
         }
         else
-            System.out.println("< Wrong! >");
+        {
+            System.out.println("< Wrong!>");
+            
+            if ( !chapter.toString().equals(currentChapter) )
+            {
+                currentChapter = chapter.toString(); // Update current chapter 
+                chapterIndex++; // Update chapter score index +1 to move to next chapter
+            }
+        }
 
         // Increment +1 every time this method is called to keep track of how many
         // questions were asked
         totalQuestions++;
     }
-
 
     // Finds the resulting score value for each chapter and section
     private void analyzeFinalResult()
@@ -129,18 +179,21 @@ public class TestAndAnalyze extends TestUtilities{
         score *= 100; // Make it a percent %
         score = Math.floor(score * 100) / 100; // Round to 2 decimal places
         
-        
-        line(50, '+'); // --line--
+        line(50, '#'); // --line--
         System.out.println("Final score: " + score + "%");
-        line(50, '+'); // --line--
+        line(50, '#'); // --line--
         
-        // Create an itemized score by chapter
-        // S7 (3/3)
-        // S14 (2/3)
-        // S23 (3/3)
-        // ---------
-        // Final: 92.5%
+        // Make the list of chapters into a string array
+        for (int x = 0; x < listOfChapters.size(); x++)
+        {
+            // Print chapter name
+            System.out.print(listOfChapters.get(x));
+            
+            // Bar graph of chapter score
+            barGraphLine(chapterScore[x], '=', 3);
+        }
+        
+        line(50, '#'); // --line--
     }
-
 }
 
