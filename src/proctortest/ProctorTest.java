050974e06724ffa25f669432324b9cddc9e9157d
Brandon Jumbeck <bjumbeck@outlook.com>
2018-12-06 15:24:26 -0600

    Fixed the infinite loop that happened after the menu told it to close.
    It was caused by the threads in the background keeping the program open.

--- src/proctortest/ProctorTest.java
+++ src/proctortest/ProctorTest.java
@@ -6,20 +6,29 @@ import java.util.Scanner;
 
 /**
  * @date 11-21-18
- * @authors (Paul Egbe, Kyle Blaha, Thanh Nguyen, Mackenzie Branch, Insert group names)
+ * @author (Paul Egbe, Kyle Blaha, Thanh Nguyen, Mackenzie Branch, Insert group names)
  **/
 public class ProctorTest {
     public static void main(String[] args) {
         boolean retakeSameTest = false;
         boolean takeANewTest = true; // True just to enter the test at least once.
 
-        // Create a QuestionBank object which holds all the questions possible, it gets those questions
-        // from a text file called questionBank.txt. Initializing an object will automatically parse that file.
-        QuestionBank questionBank = new QuestionBank(new File("questionBank.txt"));
+        // Load the questions from the text file into an array concurrently.
+        FileManager fileManager = new FileManager();
+        ArrayList<Question> allQuestions;
+        try {
+            allQuestions = fileManager.loadQuestionBankConcurrent(new File("questionBank.txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Create a QuestionBank object which holds all the questions possible.
+        QuestionBank questionBank = new QuestionBank(allQuestions);
 
         // Create a brand new test with 3 randomly generated questions per chapter stored in our QuestionBank
         ArrayList<Question> questionList = questionBank.generateQuestionList(3);
-        Test test = new Test(questionList);
+        Test test = new Test(questionList, fileManager);
 
         do {
             if (takeANewTest) {
@@ -27,7 +36,19 @@ public class ProctorTest {
                 // Create a brand new test with 3 randomly generated questions per chapter stored in our QuestionBank
                 // There is some inefficiency here with creating another new test member.
                 questionList = questionBank.generateQuestionList(3);
-                test = new Test(questionList);
+                test = new Test(questionList, fileManager);
+
+                // Get user information so that we can save the test results
+                String userName;
+                do {
+                    System.out.print("Please enter your first and last name with a space in between: ");
+                    Scanner scanner = new Scanner(System.in);
+                    userName = scanner.nextLine();
+                } while (!userName.matches("[a-zA-Z]*[\\s]{1}[a-zA-Z].*") || userName.isEmpty());
+
+                fileManager.setUsernameForFile(userName);
+                System.out.println("\n\n\n");
+
                 test.runTest();
                 test.analyzeFinalResult();
 
@@ -63,6 +84,14 @@ public class ProctorTest {
                 // Retake the same test that they just took, so no need to generate new random questions.
                 test.runTest();
                 test.analyzeFinalResult();
+
+                // Get user information so that we can save the test results
+                String userName;
+                do {
+                    System.out.print("Please enter your first and last name with a space in between: ");
+                    Scanner scanner = new Scanner(System.in);
+                    userName = scanner.nextLine();
+                } while (!userName.matches("[a-zA-Z]*[\\s]{1}[a-zA-Z].*") || userName.isEmpty());
 
                 // TODO: If we have time, refactor this menu into something that isn't copy and pasted in both conditions
                 int menuChoice;
